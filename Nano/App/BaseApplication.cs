b0b1332cdc294df18ca1417518a600054d345dae
vivet <vivet@vivetonline.com>
2017-09-27 01:54:55 +0200

    Added Todo's

--- Nano/App/BaseApplication.cs
+++ Nano/App/BaseApplication.cs
@@ -17,7 +17,10 @@ using Nano.App.Logging.Extensions;
 namespace Nano.App
 {
     // TODO: Remove scope from controllers if not needed??
-
+    // TODO: Include Startup from different assembly? 
+    
+    // TODO: Add ServiceOptions (IOtions<T>) store in DbContext so settings for the service can be set. yes / no? 
+    // TODO: Globalization and localization (https://docs.microsoft.com/en-us/aspnet/core/fundamentals/localization) 
     // TODO: Versioning / api-explorer (https://dotnetcoretutorials.com/2017/01/17/api-versioning-asp-net-core/)
     // TODO: Support for more controller action (CreateMany, EditMany, DeleteMany, and also Patch possiblities for only passing update changes to the Edit endpoint.)
     // TODO: Change tracking / onChanged events? for RabbitMQ and other triggers for calculating stats
@@ -27,7 +30,7 @@ namespace Nano.App
     // TODO: DOCKER: Fix dry-run mysql not ready (delay app start)
     // TODO: Implement generic views, using reflection to show view model for 'Index', 'Create', 'Edit' and 'Delete'
     // TODO: For xml request / response the querystring parameter "Format=xml" is required, should look at contenttype.
-
+	
     /// <summary>
     /// Base Application (abstract).
     /// </summary>
