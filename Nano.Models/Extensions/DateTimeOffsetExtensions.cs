414260a90df365ac0e0f7ab03ff84d8f06848189
vivet <vivet@vivetonline.com>
2018-03-18 10:41:42 +0100

    Added DateTime Extension methods for start and finish dates in intervals.

--- Nano.Models/Extensions/DateTimeOffsetExtensions.cs
+++ Nano.Models/Extensions/DateTimeOffsetExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Globalization;
+using Nano.Models.Extensions.Enums;
 
 namespace Nano.Models.Extensions
 {
@@ -9,29 +10,96 @@ namespace Nano.Models.Extensions
     public static class DateTimeOffsetExtensions
     {
         /// <summary>
-        /// Gets the first and last day of the month of the passed <see cref="DateTimeOffset"/> instance.
+        /// Gets the week number.
         /// </summary>
-        /// <param name="dateTime">The <see cref="DateTimeOffset"/>.</param>
-        /// <returns>The first and last day of the month of the passed <see cref="DateTimeOffset"/>.</returns>
-        public static (DateTimeOffset, DateTimeOffset) GetMonthDays(this DateTimeOffset dateTime)
+        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
+        /// <returns>The week number if the passe <see cref="DateTimeOffset"/>.</returns>
+        public static int GetWeekNumber(this DateTimeOffset at)
         {
-            var firstDayOfMonth = new DateTime(dateTime.Year, dateTime.Month, 1);
-            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+            var dfi = DateTimeFormatInfo.CurrentInfo;
+            var weekNumber = dfi.Calendar.GetWeekOfYear(at.DateTime, dfi.CalendarWeekRule, dfi.FirstDayOfWeek);
 
-            return (firstDayOfMonth, lastDayOfMonth);
+            return weekNumber;
         }
 
         /// <summary>
-        /// 
+        /// Get the start and end date of the day, matching the passed <see cref="DateTimeOffset"/>.
         /// </summary>
-        /// <param name="dateTime"></param>
-        /// <returns></returns>
-        public static int GetWeekNumber(this DateTimeOffset dateTime)
+        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
+        /// <returns>The start and end date.</returns>
+        public static (DateTimeOffset, DateTimeOffset) GetDayDates(this DateTimeOffset at)
+        {
+            var startAt = at.Subtract(at.TimeOfDay);
+            var endAt = startAt.AddSeconds(-1);
+
+            return (startAt, endAt);
+        }
+
+        /// <summary>
+        /// Get the start and end date of the week, matching the passed <see cref="DateTimeOffset"/>.
+        /// </summary>
+        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
+        /// <returns>The start and end date.</returns>
+        public static (DateTimeOffset, DateTimeOffset) GetWeekDates(this DateTimeOffset at)
         {
             var dfi = DateTimeFormatInfo.CurrentInfo;
-            var weekOfYear = dfi.Calendar.GetWeekOfYear(dateTime.DateTime, dfi.CalendarWeekRule, dfi.FirstDayOfWeek);
+            var startAt = at.AddDays(-(at.DayOfWeek - dfi.FirstDayOfWeek)).Subtract(at.TimeOfDay);
+            var endAt = startAt.AddDays(7).AddSeconds(-1);
 
-            return weekOfYear;
+            return (startAt, endAt);
+        }
+
+        /// <summary>
+        /// Gets the start and end date of the month, matching the passed <see cref="DateTimeOffset"/> instance.
+        /// </summary>
+        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
+        /// <returns>The start and end dates.</returns>
+        public static (DateTimeOffset, DateTimeOffset) GetMonthDates(this DateTimeOffset at)
+        {
+            var startAt = new DateTimeOffset(at.Year, at.Month, 1, 0, 0, 0, new TimeSpan());
+            var endAt = startAt.AddMonths(1).AddSeconds(-1);
+
+            return (startAt, endAt);
+        }
+
+        /// <summary>
+        /// Gets the start and end date of the year, matching the passed <see cref="DateTimeOffset"/> instance.
+        /// </summary>
+        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
+        /// <returns>The start and end date.</returns>
+        public static (DateTimeOffset, DateTimeOffset) GetYearDates(this DateTimeOffset at)
+        {
+            var startAt = new DateTimeOffset(at.Year, 1, 1, 0, 0, 0, new TimeSpan());
+            var endAt = startAt.AddYears(1).AddSeconds(-1);
+
+            return (startAt, endAt);
+        }
+
+        /// <summary>
+        /// Gets the start and end date of the <see cref="DateInterval"/> passed, matching the passed <see cref="DateTimeOffset"/>.
+        /// </summary>
+        /// <param name="dateInterval">The <see cref="DateInterval"/>.</param>
+        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
+        /// <returns>The start and end date.</returns>
+        public static (DateTimeOffset, DateTimeOffset) GetDates(this DateTimeOffset at, DateInterval dateInterval)
+        {
+            switch (dateInterval)
+            {
+                case DateInterval.DAYLY:
+                    return at.GetDayDates();
+
+                case DateInterval.WEEKLY:
+                    return at.GetWeekDates();
+
+                case DateInterval.MONTHLY:
+                    return at.GetMonthDates();
+
+                case DateInterval.YEARLY:
+                    return at.GetYearDates();
+
+                default:
+                    return (at.Date, at.Date);
+            }
         }
     }
 }
---
+++ Nano.Models/Extensions/Enums/DateInterval.cs
@@ -0,0 +1,28 @@
+namespace Nano.Models.Extensions.Enums
+{
+    /// <summary>
+    /// Date Interval.
+    /// </summary>
+    public enum DateInterval
+    {
+        /// <summary>
+        /// Daily.
+        /// </summary>
+        DAYLY,
+
+        /// <summary>
+        /// Weekly.
+        /// </summary>
+        WEEKLY,
+
+        /// <summary>
+        /// Monthly.
+        /// </summary>
+        MONTHLY,
+
+        /// <summary>
+        /// Yearly.
+        /// </summary>
+        YEARLY
+    }
+}
