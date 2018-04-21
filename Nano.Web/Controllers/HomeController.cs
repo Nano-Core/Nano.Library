08d0f1b25d7bb1b872c3aaab21f0278a177f36a2
vivet <vivet@vivetonline.com>
2018-04-21 23:56:05 +0200

    Updated controllers (removed several from home - not needed)

--- Nano.Web/Controllers/HomeController.cs
+++ Nano.Web/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Net;
-using System.Threading;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Nano.Models;
 using Nano.Web.Hosting;
@@ -37,61 +33,6 @@ namespace Nano.Web.Controllers
         }
 
         /// <summary>
-        /// Ping the application. Is it alive.
-        /// </summary>
-        /// <returns>Nothing (void).</returns>
-        /// <response code="200">Success.</response>
-        [HttpGet]
-        [Route("ping")]
-        [Produces(HttpContentType.JSON, HttpContentType.XML, HttpContentType.HTML)]
-        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
-        public virtual IActionResult Ping()
-        {
-            if (this.Request.IsContentTypeHtml())
-                return this.View();
-
-            return Ok();
-        }
-
-        /// <summary>
-        /// Gets the http options.
-        /// </summary>
-        /// <returns>The http options.</returns>
-        /// <response code="200">Success.</response>
-        [HttpOptions]
-        [Route("options")]
-        [Produces(HttpContentType.JSON, HttpContentType.XML, HttpContentType.HTML)]
-        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
-        public virtual IActionResult Options()
-        {
-            // FEATURE: Http Options: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/OPTIONS
-
-            if (this.Request.IsContentTypeHtml())
-                return this.View();
-
-            return Ok(); 
-        }
-
-        /// <summary>
-        /// Gets the supported versions of the application.
-        /// </summary>
-        /// <returns>The supported versions.</returns>
-        /// <response code="200">Success.</response>
-        [HttpGet]
-        [Route("versions")]
-        [Produces(HttpContentType.JSON, HttpContentType.XML, HttpContentType.HTML)]
-        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
-        public virtual IActionResult Versions()
-        {
-            // FEATURE: Http supported Versions.
-
-            if (this.Request.IsContentTypeHtml())
-                return this.View();
-
-            return this.Ok(); 
-        }
-
-        /// <summary>
         /// Gets the error occured.
         /// </summary>
         /// <param name="error">The error.</param>
@@ -107,48 +48,6 @@ namespace Nano.Web.Controllers
                 return this.View(error);
 
             return this.Ok(error);
-        }
-
-        /// <summary>
-        /// Sets the language used for future requests.
-        /// </summary>
-        /// <param name="code">The langauge code.</param>
-        /// <param name="returnUrl">The return url (if any).</param>
-        /// <param name="cancellationToken">The cancellationToken.</param>
-        /// <returns>Nothing (void).</returns>
-        [HttpGet]
-        [HttpPost]
-        [Route("language")]
-        [Produces(HttpContentType.JSON, HttpContentType.XML, HttpContentType.HTML)]
-        [ProducesResponseType(typeof(Error), (int)HttpStatusCode.OK)]
-        public virtual IActionResult SetLanguage([FromQuery][FromHeader(Name = "Accept-Language")][Required]string code, [FromQuery]string returnUrl, CancellationToken cancellationToken = default)
-        {
-            // FEATURE: Http Localization, how to use IRequestCultureProviders? HomeController.SetLanguage?
-
-            var cookieName = CookieRequestCultureProvider.DefaultCookieName;
-            var cookieValue = CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(code));
-            var cookieOptions = new CookieOptions
-            {
-                Expires = DateTimeOffset.UtcNow.AddYears(1)
-            };
-
-            this.Response.Cookies
-                .Append(cookieName, cookieValue, cookieOptions);
-
-            if (this.Request.IsContentTypeHtml())
-            {
-                if (returnUrl == null)
-                {
-                    if (this.Request.IsContentTypeHtml())
-                        return this.RedirectToAction("Index");
-
-                    return Ok();
-                }
-
-                return this.LocalRedirect(returnUrl);
-            }
-
-            return this.Ok();
         }
     }
 }
