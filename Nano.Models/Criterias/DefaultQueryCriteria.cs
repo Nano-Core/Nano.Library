dabb9c41fd1653956deafcc2e61b1ed3b5c32cc4
vivet <vivet@vivetonline.com>
2018-04-21 11:24:59 +0200

    Removed IsActive from DefaultQueryCriteria

--- Nano.Models/Criterias/DefaultQueryCriteria.cs
+++ Nano.Models/Criterias/DefaultQueryCriteria.cs
@@ -7,13 +7,6 @@ namespace Nano.Models.Criterias
     public class DefaultQueryCriteria : BaseQueryCriteria
     {
         /// <summary>
-        /// Is Active.
-        /// Default: true. 
-        /// WHen soft-deleted it's set to false instead of deleting data.
-        /// </summary>
-        public virtual bool IsActive { get; } = true;
-
-        /// <summary>
         /// After At.
         /// </summary>
         public virtual DateTimeOffset? AfterAt { get; set; }
@@ -27,8 +20,6 @@ namespace Nano.Models.Criterias
         public override CriteriaExpression GetExpression<TEntity>() 
         {
             var expression = base.GetExpression<TEntity>();
-
-            expression.Equal("IsActive", this.IsActive);
 
             if (this.BeforeAt.HasValue)
                 expression.LessThanOrEqual("CreatedAt", this.BeforeAt);
