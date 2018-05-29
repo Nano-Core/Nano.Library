99e8ca537a90d0a55cf88f639622144749585885
Michael Vivet <vivet@vivetonline.com>
2018-05-30 01:16:25 +0200

    Added more overloads for custom config sections

--- Nano.Config/Extensions/ServiceCollectionExtensions.cs
+++ Nano.Config/Extensions/ServiceCollectionExtensions.cs
@@ -27,12 +27,62 @@ namespace Nano.Config.Extensions
         }
 
         /// <summary>
-        /// Adds a appOptions <see cref="IConfigurationSection"/> as <see cref="IOptions{TOptions}"/> to the <see cref="IServiceCollection"/>.
+        /// Adds a appOptions <see cref="IConfigurationSection"/> as <see cref="IOptions{TOption}"/> to the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <typeparam name="TOption">The option implementation type.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="name">The name of the section.</param>
+        /// <returns>The <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddConfigOptions<TOption>(this IServiceCollection services, string name)
+            where TOption : class, new()
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+
+            return services
+                .AddConfigOptions<TOption>(name, out _);
+        }
+
+        /// <summary>
+        /// Adds a appOptions <see cref="IConfigurationSection"/> as <see cref="IOptions{TOption}"/> to the <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <typeparam name="TOption">The option implementation type.</typeparam>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="name">The name of the section.</param>
+        /// <param name="options">The options configured and loaded.</param>
+        /// <returns>The <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddConfigOptions<TOption>(this IServiceCollection services, string name, out TOption options)
+            where TOption : class, new()
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+
+            var provider = services.BuildServiceProvider();
+            var configuration = provider.GetRequiredService<IConfiguration>();
+            var section = configuration.GetSection(name);
+
+            options = section?.Get<TOption>() ?? new TOption();
+
+            services
+                .AddSingleton(options)
+                .Configure<TOption>(section);
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds a appOptions <see cref="IConfigurationSection"/> as <see cref="IOptions{TOption}"/> to the <see cref="IServiceCollection"/>.
         /// </summary>
         /// <typeparam name="TOption">The option implementation type.</typeparam>
         /// <param name="services">The <see cref="IServiceCollection"/>.</param>
         /// <param name="configuration">The <see cref="IConfiguration"/>.</param>
-        /// <param name="name">The name of the <see cref="IConfigurationSection"/>.</param>
+        /// <param name="name">The name of the section.</param>
         /// <param name="options">The options configured and loaded.</param>
         /// <returns>The <see cref="IServiceCollection"/>.</returns>
         public static IServiceCollection AddConfigOptions<TOption>(this IServiceCollection services, IConfiguration configuration, string name, out TOption options)
