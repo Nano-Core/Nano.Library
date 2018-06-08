d2800c3052940b7fe983bab9c4c6a7eaf59d3487
Michael Vivet <vivet@vivetonline.com>
2018-06-08 12:43:58 +0200

    Added SqlLite data provider.

---
+++ Nano.Data/Providers/SqlLite/SqlLiteProvider.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Nano.Data.Interfaces;
+
+namespace Nano.Data.Providers.SqlLite
+{
+    /// <summary>
+    /// Sql Lite Data Provider.
+    /// </summary>
+    public class SqlLiteProvider : IDataProvider
+    {
+        /// <summary>
+        /// Options.
+        /// </summary>
+        protected virtual DataOptions Options { get; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="options">The <see cref="DataOptions"/>.</param>
+        public SqlLiteProvider(DataOptions options)
+        {
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            this.Options = options;
+        }
+
+        /// <inheritdoc />
+        public void Configure(DbContextOptionsBuilder builder)
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+
+            var useLazyLoading = this.Options.UseLazyLoading;
+            var connectionString = this.Options.ConnectionString;
+
+            if (connectionString == null)
+                return;
+
+            builder
+                .UseLazyLoadingProxies(useLazyLoading)
+                .UseInMemoryDatabase(connectionString);
+        }
+    }
+}
