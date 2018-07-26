7190b9c4167d7ea4748b556e24a0823b284bd205
Michael Vivet <vivet@vivetonline.com>
2018-07-26 06:58:56 +0200

    Fixed not changing unique indexes for soft-delete when index is already unique

--- Nano.Data/Models/Mappings/Extensions/ModelBuilderExtensions.cs
+++ Nano.Data/Models/Mappings/Extensions/ModelBuilderExtensions.cs
@@ -51,6 +51,9 @@ namespace Nano.Data.Models.Mappings.Extensions
                 .ToList()
                 .ForEach(x =>
                 {
+                    if (x.IsUnique)
+                        return;
+
                     entity.Metadata
                         .RemoveIndex(x.Properties);
 
