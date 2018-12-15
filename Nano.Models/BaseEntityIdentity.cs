9da5a1d130aacfa5a7660e56f4c8588f55ccf27c
Michael Vivet <vivet@vivetonline.com>
2018-12-15 10:08:46 +0100

    Commented out GetHascode(), that gave issues

--- Nano.Models/BaseEntityIdentity.cs
+++ Nano.Models/BaseEntityIdentity.cs
@@ -8,21 +8,23 @@ namespace Nano.Models
         /// <inheritdoc />
         public virtual TIdentity Id { get; set; }
 
-        /// <inheritdoc />
-        public override int GetHashCode()
-        {
-            // ReSharper disable NonReadonlyMemberInGetHashCode
-            return this.Id.GetHashCode();
-            // ReSharper restore NonReadonlyMemberInGetHashCode
-        }
+        // BUG: Fix GetHasCode (in net core 2.2)
+        ///// <inheritdoc />
+        //public override int GetHashCode()
+        //{
+        //    // ReSharper disable NonReadonlyMemberInGetHashCode
+        //    // ReSharper restore NonReadonlyMemberInGetHashCode
+            
+        //    return this.Id.GetHashCode();
+        //}
 
-        /// <inheritdoc />
-        public override bool Equals(object entity)
-        {
-            if (entity is IEntityIdentity<TIdentity> identity)
-                return this.Id.Equals(identity.Id);
+        ///// <inheritdoc />
+        //public override bool Equals(object entity)
+        //{
+        //    if (entity is IEntityIdentity<TIdentity> identity)
+        //        return this.Id.Equals(identity.Id);
 
-            return object.ReferenceEquals(this, entity);
-        }
+        //    return object.ReferenceEquals(this, entity);
+        //}
     }
 }
