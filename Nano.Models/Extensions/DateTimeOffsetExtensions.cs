e0cc5cb806aaca9277a7b848d7022fb66c08bb20
Michael Vivet <vivet@vivetonline.com>
2019-11-04 10:52:36 +0100

    Cleanup unused code. Updated NuGets

--- Nano.Models/Extensions/DateTimeOffsetExtensions.cs
+++ Nano.Models/Extensions/DateTimeOffsetExtensions.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Globalization;
-using Nano.Models.Extensions.Enums;
 
 namespace Nano.Models.Extensions
 {
@@ -19,99 +17,6 @@ namespace Nano.Models.Extensions
         public static long GetEpochTime(this DateTimeOffset at)
         {
             return (long)(at - DateTimeOffsetExtensions.epoch).TotalMilliseconds;
-        }
-
-        /// <summary>
-        /// Gets the week number.
-        /// </summary>
-        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
-        /// <returns>The week number if the passe <see cref="DateTimeOffset"/>.</returns>
-        public static int GetWeekNumber(this DateTimeOffset at)
-        {
-            var dfi = DateTimeFormatInfo.CurrentInfo;
-            var weekNumber = dfi?.Calendar.GetWeekOfYear(at.DateTime, dfi.CalendarWeekRule, dfi.FirstDayOfWeek) ?? 0;
-
-            return weekNumber;
-        }
-
-        /// <summary>
-        /// Get the start and end date of the day, matching the passed <see cref="DateTimeOffset"/>.
-        /// </summary>
-        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
-        /// <returns>The start and end date.</returns>
-        public static (DateTimeOffset, DateTimeOffset) GetDayDates(this DateTimeOffset at)
-        {
-            var startAt = new DateTimeOffset(at.Year, at.Month, at.Day, 0, 0, 0, new TimeSpan());
-            var endAt = startAt.AddDays(1).AddSeconds(-1);
-
-            return (startAt, endAt);
-        }
-
-        /// <summary>
-        /// Get the start and end date of the week, matching the passed <see cref="DateTimeOffset"/>.
-        /// </summary>
-        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
-        /// <returns>The start and end date.</returns>
-        public static (DateTimeOffset, DateTimeOffset) GetWeekDates(this DateTimeOffset at)
-        {
-            var dfi = DateTimeFormatInfo.CurrentInfo;
-            var startAt = at.AddDays(-(at.DayOfWeek - dfi?.FirstDayOfWeek ?? 0)).Subtract(at.TimeOfDay);
-            var endAt = startAt.AddDays(7).AddSeconds(-1);
-
-            return (startAt, endAt);
-        }
-
-        /// <summary>
-        /// Gets the start and end date of the month, matching the passed <see cref="DateTimeOffset"/> instance.
-        /// </summary>
-        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
-        /// <returns>The start and end dates.</returns>
-        public static (DateTimeOffset, DateTimeOffset) GetMonthDates(this DateTimeOffset at)
-        {
-            var startAt = new DateTimeOffset(at.Year, at.Month, 1, 0, 0, 0, new TimeSpan());
-            var endAt = startAt.AddMonths(1).AddSeconds(-1);
-
-            return (startAt, endAt);
-        }
-
-        /// <summary>
-        /// Gets the start and end date of the year, matching the passed <see cref="DateTimeOffset"/> instance.
-        /// </summary>
-        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
-        /// <returns>The start and end date.</returns>
-        public static (DateTimeOffset, DateTimeOffset) GetYearDates(this DateTimeOffset at)
-        {
-            var startAt = new DateTimeOffset(at.Year, 1, 1, 0, 0, 0, new TimeSpan());
-            var endAt = startAt.AddYears(1).AddSeconds(-1);
-
-            return (startAt, endAt);
-        }
-
-        /// <summary>
-        /// Gets the start and end date of the <see cref="DateInterval"/> passed, matching the passed <see cref="DateTimeOffset"/>.
-        /// </summary>
-        /// <param name="dateInterval">The <see cref="DateInterval"/>.</param>
-        /// <param name="at">The <see cref="DateTimeOffset"/>.</param>
-        /// <returns>The start and end date.</returns>
-        public static (DateTimeOffset, DateTimeOffset) GetDates(this DateTimeOffset at, DateInterval dateInterval)
-        {
-            switch (dateInterval)
-            {
-                case DateInterval.Dayly:
-                    return at.GetDayDates();
-
-                case DateInterval.Weekly:
-                    return at.GetWeekDates();
-
-                case DateInterval.Monthly:
-                    return at.GetMonthDates();
-
-                case DateInterval.Yearly:
-                    return at.GetYearDates();
-
-                default:
-                    return (at.Date, at.Date);
-            }
         }
     }
 }
