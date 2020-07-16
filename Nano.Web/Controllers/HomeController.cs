b4b756e9c67839d4ec8f530e6461099022d80d9a
Michael Vivet <vivet@vivetonline.com>
2020-07-16 10:11:06 +0200

    Added get-services to HomeController

--- Nano.Web/Controllers/HomeController.cs
+++ Nano.Web/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Net;
 using System.Threading;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Nano.Security.Const;
 using Nano.Web.Const;
 using Nano.Web.Models;
@@ -22,18 +26,73 @@ namespace Nano.Web.Controllers
     public class HomeController : BaseController
     {
         /// <summary>
+        /// Logger.
+        /// </summary>
+        protected virtual ILogger Logger { get; }
+
+        /// <summary>
+        /// Services.
+        /// </summary>
+        protected virtual IServiceCollection Services { get; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="logger">The <see cref="ILogger"/>.</param>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        public HomeController(ILogger logger, IServiceCollection services)
+        {
+            this.Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.Services = services ?? throw new ArgumentNullException(nameof(services));
+        }
+
+        /// <summary>
         /// Default action (ping).
         /// </summary>
         /// <returns>Void.</returns>
         /// <response code="200">OK.</response>
         /// <response code="500">Error occured.</response>
         [HttpGet]
-        [Route("index")]
+        [Route("ping")]
+        [AllowAnonymous]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(Error), (int)HttpStatusCode.InternalServerError)]
-        public virtual IActionResult Index()
+        public virtual IActionResult Ping()
         {
             return this.Ok();
+        }
+
+        /// <summary>
+        /// Get registered 
+        /// </summary>
+        /// <param name="filter">The filter on service or implementation name.</param>
+        /// <returns>Void.</returns>
+        /// <response code="200">OK.</response>
+        /// <response code="401">Unauthorized.</response>
+        /// <response code="404">Not Found.</response>
+        /// <response code="500">Error occured.</response>
+        [HttpGet]
+        [Route("services")]
+        [Produces(HttpContentType.JSON, HttpContentType.XML)]
+        [ProducesResponseType(typeof(IEnumerable<ServiceDependency>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType(typeof(Error), (int)HttpStatusCode.InternalServerError)]
+        public virtual IActionResult GetServices([FromQuery]string filter = "")
+        {
+            var serviceDescriptors = this.Services
+                .Where(x => 
+                    x.ServiceType?.FullName != null && x.ServiceType.FullName.Contains(filter) ||
+                    x.ImplementationType?.FullName != null && x.ImplementationType.FullName.Contains(filter))
+                .Select(x => new ServiceDependency
+                {
+                    Service = x.ServiceType.FullName, 
+                    Implementation = x.ImplementationType?.FullName, 
+                    LifeTime = x.Lifetime
+                })
+                .OrderBy(x => x.Service);
+
+            return this.Ok(serviceDescriptors);
         }
 
         /// <summary>
@@ -46,6 +105,7 @@ namespace Nano.Web.Controllers
         /// <response code="500">Error occured.</response>
         [HttpPost]
         [Route("language")]
+        [AllowAnonymous]
         [Consumes(HttpContentType.JSON, HttpContentType.XML)]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(Error), (int)HttpStatusCode.InternalServerError)]
@@ -74,6 +134,7 @@ namespace Nano.Web.Controllers
         /// <response code="500">Error occured.</response>
         [HttpPost]
         [Route("timezone")]
+        [AllowAnonymous]
         [Consumes(HttpContentType.JSON, HttpContentType.XML)]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         [ProducesResponseType(typeof(Error), (int)HttpStatusCode.InternalServerError)]
