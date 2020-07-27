540a40d33c4cf48de3a9d64a3f930c29706d2cbd
Michael Vivet <vivet@vivetonline.com>
2020-07-27 08:00:42 +0200

    Updated Identity models (final)

--- Nano.Models/BaseEntityUser.cs
+++ Nano.Models/BaseEntityUser.cs
@@ -1,4 +1,3 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 using Nano.Models.Attributes;
@@ -6,23 +5,14 @@ using Nano.Models.Interfaces;
 
 namespace Nano.Models
 {
-    /// <inheritdoc />
-    public class BaseEntityUser : BaseEntityUser<Guid>
-    {
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        public BaseEntityUser()
-        {
-            this.Id = Guid.NewGuid();
-        }
-    }
-
     /// <summary>
     /// Base Entity User (abstract).
     /// </summary>
-    public abstract class BaseEntityUser<TIdentity> : BaseEntityIdentity<TIdentity>, IEntityWritable, IEntityUser<TIdentity>
+    public abstract class BaseEntityUser<TIdentity> : BaseEntityIdentity<TIdentity>, IEntityUser<TIdentity>, IEntityWritable
     {
+        /// <inheritdoc />
+        public virtual long IsDeleted { get; set; } = 0L;
+
         /// <inheritdoc />
         [MaxLength(128)]
         public virtual string IdentityUserId { get; set; }
@@ -30,13 +20,5 @@ namespace Nano.Models
         /// <inheritdoc />
         [Include]
         public virtual IdentityUser IdentityUser { get; set; }
-
-        /// <inheritdoc />
-        public virtual long IsDeleted { get; set; } = 0L;
-
-        /// <summary>
-        /// Created At.
-        /// </summary>
-        public virtual DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
     }
 }
--- Nano.Models/DefaultEntityUser.cs
+++ Nano.Models/DefaultEntityUser.cs
@@ -1,12 +1,26 @@
 using System;
+using Nano.Models.Interfaces;
 
 namespace Nano.Models
 {
     /// <summary>
     /// Default Entity User.
     /// </summary>
-    public class DefaultEntityUser : BaseEntityUser<Guid>
+    public class DefaultEntityUser : DefaultEntityUser<Guid>
     {
+        /// <inheritdoc />
+        protected DefaultEntityUser()
+        {
+            this.Id = Guid.NewGuid();
+        }
+    }
 
+    /// <inheritdoc cref="IEntityWritable"/>
+    public class DefaultEntityUser<TIdentity> : BaseEntityUser<TIdentity>
+    {
+        /// <summary>
+        /// Created At.
+        /// </summary>
+        public virtual DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
     }
 }
