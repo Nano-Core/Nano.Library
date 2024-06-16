7452a5613ba5f7889b2b1e89636449ba4bab32d1
Michael Vivet <miv@parseport.com>
2024-06-16 12:18:23 +0200

    Changed static HttpContextAccessor to public visibility

--- Nano.App/HttpContextAccessor.cs
+++ Nano.App/HttpContextAccessor.cs
@@ -6,7 +6,7 @@ namespace Nano.App;
 /// <summary>
 /// Http Context Access (Static)
 /// </summary>
-internal static class HttpContextAccessor
+public static class HttpContextAccessor
 {
     private static IHttpContextAccessor accessor;
 
@@ -14,13 +14,13 @@ internal static class HttpContextAccessor
     /// Current.
     /// The current <see cref="HttpContext"/>, fetched through the <see cref="IHttpContextAccessor"/>.
     /// </summary>
-    internal static HttpContext Current => accessor?.HttpContext;
+    public static HttpContext Current => accessor?.HttpContext;
 
     /// <summary>
     /// Configure.
     /// </summary>
     /// <param name="httpContextAccessor">The <inheritdoc cref="IHttpContextAccessor"/>.</param>
-    internal static void Configure(IHttpContextAccessor httpContextAccessor)
+    public static void Configure(IHttpContextAccessor httpContextAccessor)
     {
         HttpContextAccessor.accessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
     }
