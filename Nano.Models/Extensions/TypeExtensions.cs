4c385a139bb6e3790068b155656a6c008accf8d1
vivet <vivet@vivetonline.com>
2024-07-23 15:45:19 +0200

    Added more simple types

--- Nano.Models/Extensions/TypeExtensions.cs
+++ Nano.Models/Extensions/TypeExtensions.cs
@@ -66,7 +66,12 @@ public static class TypeExtensions
         return type.IsPrimitive
                || type.IsEnum
                || type == typeof(string)
-               || type == typeof(decimal);
+               || type == typeof(DateTime)
+               || type == typeof(DateTimeOffset)
+               || type == typeof(TimeSpan)
+               || type == typeof(Guid)
+               || type == typeof(DateTime)
+               || type == typeof(Nullable<>);
     }
 
     private static string GetTypeString(this Type type)
