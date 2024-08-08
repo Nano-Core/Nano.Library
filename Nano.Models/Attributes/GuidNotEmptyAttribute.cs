7e4f6732ac9edcf98369a9924d0acf12054c5e33
vivet <vivet@vivetonline.com>
2024-08-08 16:10:30 +0200

    Added GuidNotEmpty Attribute

---
+++ Nano.Models/Attributes/GuidNotEmptyAttribute.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nano.Models.Attributes;
+
+/// <summary>
+/// Guid Not Empty Attribute.
+/// </summary>
+public class GuidNotEmptyAttribute : ValidationAttribute
+{
+    /// <inheritdoc />
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is Guid guid && guid == Guid.Empty)
+        {
+            return new ValidationResult("The GUID must not be empty.");
+        }
+
+        return ValidationResult.Success;
+    }
+}
