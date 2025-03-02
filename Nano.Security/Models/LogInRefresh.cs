8213433e6c56f5cc9a1e7ed59904eb0e97c7d3c6
vivet <vivet@vivetonline.com>
2025-03-02 09:55:47 +0100

    Rename

---
+++ Nano.Security/Models/LogInRefresh.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nano.Security.Models;
+
+/// <summary>
+/// Log In Refresh.
+/// </summary>
+public class LogInRefresh
+{
+    /// <summary>
+    /// Token.
+    /// </summary>
+    [Required]
+    public virtual string Token { get; set; }
+
+    /// <summary>
+    /// Refresh Token.
+    /// </summary>
+    [Required]
+    public virtual string RefreshToken { get; set; }
+
+    /// <summary>
+    /// Transient Roles.
+    /// Non persisted roles, that is added to the jwt-token when logging in.
+    /// </summary>
+    public virtual IEnumerable<string> TransientRoles { get; set; } = new List<string>();
+
+    /// <summary>
+    /// Transient Claims.
+    /// Non persisted claims, that is added to the jwt-token when logging in.
+    /// </summary>
+    public virtual IDictionary<string, string> TransientClaims { get; set; } = new Dictionary<string, string>();
+}
